865497f410bfbce002aea78905c21d3da8095ae4
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2026-06-22 15:12:38 +0200

    following surface instructions for fedora silverblue but with dnf

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,16 @@ cp -avf "/ctx/system_files"/. /
 # Install surface-kernel
 dnf5 config-manager -y \
     addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
-dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs zram-generator-defaults
-# rm -rf /boot/* /usr/lib/modules/* /lib/modules/*
-dnf5 -y --setopt=tsflags=noscripts install kernel-surface
-dnf5 -y install --allowerasing iptsd libwacom-surface
+dnf5 -y remove --no-autoremove \
+       	kernel-core \
+       	kernel-modules \
+	kernel-modules-extra \
+
+wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
+dnf5 -y install --setopt=tsflags=noscripts ./*.rpm
+
+dnf5 -y install --setopt=tsflags=noscripts kernel-surface
+dnf5 -y install --allowerasing iptsd libwacom-surface libwacom-surface-data
 
 # Enabling a System Unit Files
 
