47f36bf979b729f85785e00dee7b443819c8be86
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2026-06-22 14:36:06 +0200

    trying without removing files

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ cp -avf "/ctx/system_files"/. /
 dnf5 config-manager -y \
     addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs zram-generator-defaults
-rm -rf /boot/* /usr/lib/modules/* /lib/modules/*
+# rm -rf /boot/* /usr/lib/modules/* /lib/modules/*
 dnf5 -y --setopt=tsflags=noscripts install kernel-surface
 dnf5 -y install --allowerasing iptsd libwacom-surface
 
