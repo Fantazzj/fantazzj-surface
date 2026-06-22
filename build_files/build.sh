0ec9339dcfc270bae1493f110db891f4420d97f5
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2026-06-22 22:48:58 +0200

    added initramfs generation, but it fails something during generation

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,18 +8,47 @@ cp -avf "/ctx/system_files"/. /
 ### Install packages
 
 # Install surface-kernel
-dnf5 config-manager -y \
-    addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
-dnf5 -y remove --no-autoremove \
-       	kernel-core \
-       	kernel-modules \
-	kernel-modules-extra \
+curl -Lo /etc/yum.repos.d/linux-surface.repo https://pkg.surfacelinux.com/fedora/linux-surface.repo 
 
-wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
-dnf5 -y install --setopt=tsflags=noscripts ./*.rpm
+#dnf5 -y download --destdir=/tmp/kernel-rpms \
+#  kernel-surface \
+#  kernel-surface-core \
+#  kernel-surface-modules \
+#  kernel-surface-modules-core \
+#  kernel-surface-modules-extra \
+#  kernel-surface-devel-matched \
+#  kernel-surface-default-watchdog \
+#  kernel-uki-virt \
+#  libwacom-surface \
+#  libwacom-surface-data \
+#  iptsd
 
-dnf5 -y install --setopt=tsflags=noscripts kernel-surface
-dnf5 -y install --allowerasing iptsd libwacom-surface libwacom-surface-data
+#rpm-ostree override replace \
+#  --experimental \
+#  --remove kernel \
+#  --remove kernel-core \
+#  --remove kernel-modules \
+#  --remove kernel-modules-core \
+#  --remove kernel-modules-extra \
+#  --remove kernel-devel-matched \
+#  --remove libwacom \
+#  --remove libwacom-data \
+#  /tmp/kernel-rpms/kernel-surface-*.rpm \
+#  /tmp/kernel-rpms/libwacom-surface*.rpm \
+#  /tmp/kernel-rpms/iptsd*.rpm
+
+dnf5 -y remove kernel*
+dnf5 -y install --allowerasing --setopt=tsflags=noscripts \
+  kernel-surface \
+  kernel-surface-devel \
+  iptsd \
+  libwacom-surface
+
+KERNEL_SUFFIX="surface"
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+depmod "$QUALIFIED_KERNEL"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 
 # Enabling a System Unit Files
 
